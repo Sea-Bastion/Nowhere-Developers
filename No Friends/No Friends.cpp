0ae96735e0b547134754b130b193d0c157da886f
Sebastian Cypert <Sebastian.Cypert@gmail.com>
2018-03-01 10:26:14 -0600

    comments and a couple little modifications

--- No Friends/No Friends.cpp
+++ No Friends/No Friends.cpp
@@ -12,16 +12,18 @@ struct Friends{
 };
 
 
-char line[15];
-bool eof, none = true;
+bool eof;
 vector<Friends> allData;
 
 int main(){
 
 	while(!eof){
-		vector<string> input;
 
+
+		char line[15];
+		vector<string> input;
 		Friends testCase;
+
 
 		//getting n and setting up array
 		cin.getline(line, 15);
@@ -30,8 +32,8 @@ int main(){
 
 
 
+		//getting input from stdin
 		cin.getline(line, 15);
-		//getting input
 		while (*line && !eof){
 			input.push_back(line);
 			if(cin.eof())
@@ -45,9 +47,12 @@ int main(){
 
 		//testing input
 		for (string i : input){
+
+			//split string and convert to int
 			int a = stoi(strtok((char *)i.c_str(), " "));
 			int b = stoi(strtok(nullptr, " "));
 
+			//you can't be friends with yourself
 			if (a != b){
 				testCase.HaveFriends[a] = true;
 				testCase.HaveFriends[b] = true;
@@ -63,21 +68,27 @@ int main(){
 	
 	//output
 	for(Friends testCase: allData){
+		bool none = true;
+
+		//go though each testCase and check for friendless numbers
 		for(int i = 1; i <= testCase.n; i++){
 			if(!testCase.HaveFriends[i]){
+
 				cout << i << " ";
 				if(none) none = false;
+
 			}
 		}
 
+		//print none if they all have friends
 		if(none){
 			cout << "NONE";
 		}
 
+		
 		delete[] testCase.HaveFriends;
 		cout << endl;
 		cout << endl;
-		none = true;
 	}
 
 }
